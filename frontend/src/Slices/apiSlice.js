0d06381740e7e9a37292d57ee45f4643a8fe7f4d
Ritik <120380082+ritikdhiman18@users.noreply.github.com>
2024-07-04 18:17:08 +0530

    baseurl updated

--- frontend/src/Slices/apiSlice.js
+++ frontend/src/Slices/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-const baseQuery = fetchBaseQuery({ baseUrl: '' })
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://primekart.onrender.com' })
 const USER_URL = '/api/users';
 export const apiSlice = createApi({
     baseQuery,
