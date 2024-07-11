3fdc7edfac9347b83415298aa5156c360a519991
Ritik <120380082+ritikdhiman18@users.noreply.github.com>
2024-07-11 14:57:53 +0530

    primecart CORS updated

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5000',
     'http://localhost:3000',
+    'https://primekart.vercel.app',
 ];
 app.use(cors({
     origin: (origin, callback) => {
