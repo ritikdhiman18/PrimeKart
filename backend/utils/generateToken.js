d8e6e46b44a5c7fb0cc5f03720ff665d073f7b37
Ritik <120380082+ritikdhiman18@users.noreply.github.com>
2024-07-22 15:05:38 +0530

    qereq

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,7 +3,7 @@ const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' })
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV != 'development',
+        secure: true,
         sameSite: 'None',
         maxAge: 30 * 24 * 60 * 60 * 1000
     })
